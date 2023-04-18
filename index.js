e74ea64bb8ec08e944f8d7ce539f2303e8095554
Jang Byung Chan <zzangno3@gmail.com>
2023-04-18 20:47:07 +0900

    환경설정

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { User } = require("./model/User");
+const config = require("./config/key");
 const port = 5000;
 
 //몽구스 연결
 mongoose
-  .connect(
-    "mongodb+srv://root:1234@bitnari.coqvziz.mongodb.net/bitnari?retryWrites=true&w=majority",
-    {}
-  )
+  .connect(config.mongoURI, {})
   .then(() => console.log("mongoose conneted"))
   .catch(() => console.log("error"));
 
+//application/x-wwww-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!~~~ 안녕하세요");
+  res.send("Hello World!~~~ 안녕하세요......");
+});
+
+app.post("/user/register", async (req, res) => {
+  console.log(`register ${JSON.stringify(req.body)}`);
+  const user = new User(req.body);
+
+  const result = await user.save().then(
+    () => {
+      return {
+        success: true,
+      };
+    },
+    (reason) => {
+      return {
+        success: false,
+        reason: reason,
+      };
+    }
+  );
+  console.log(result);
+  return res.json(result);
 });
 
 app.listen(port, () => {
